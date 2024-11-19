4a22d150bf86bbdd66f0059d97f8ab3608cb8e4f
Ryan Thomas Payne <50511769+Ry0511@users.noreply.github.com>
2024-11-19 01:12:43 +0000

    basic cmake setup alongside auto-package build target.

--- create_zip_release.py
+++ create_zip_release.py
@@ -1,81 +1,92 @@
 import argparse
+from enum import Enum
 import os
-import sys
-from zipfile import ZIP_DEFLATED, ZipFile
+from os import path
+from typing import AnyStr
+from zipfile import ZipFile, ZIP_DEFLATED
 
-# - NOTE -
-# This generates a PythonSDK-X.Y.Z.zip package for release. Embedding all required and some optional
-# files but which are still useful.
-#
 
-__version__: str
-__author__: str
-__zip_file__: str
-__exclude_paths__: list[str] = ["unrealsdk", "pyunrealsdk"]
+class BuildType(Enum):
+    Empty = ""
+    Debug = "Debug"
+    Release = "Release"
+    Unknown = "Custom"
+
+    def file_prefix(self):
+        if self is BuildType.Empty:
+            return ""
+        else:
+            return f"-{self.name}"
 
 
 ################################################################################
 # | UTILITY |
 ################################################################################
 
-def is_valid_file(path):
-    if not os.path.isfile(os.path.abspath(path)):
-        raise argparse.ArgumentTypeError(f"File '{path}' doesn't exist")
-    return os.path.abspath(path)
+def is_valid_file(in_path: AnyStr) -> AnyStr:
+    if not path.isfile(path.abspath(in_path)):
+        raise argparse.ArgumentTypeError(f"File '{in_path}' doesn't exist")
+    return path.abspath(in_path)
 
 
-def is_valid_dir(path):
-    if not os.path.isdir(os.path.abspath(path)):
-        raise argparse.ArgumentTypeError(f"Directory '{path}' doesn't exist")
-    return os.path.abspath(path)
+def dir_exists(in_path: AnyStr) -> AnyStr:
+    if not path.isdir(path.abspath(in_path)):
+        raise argparse.ArgumentTypeError(f"Directory '{in_path}' doesn't exist")
+    return path.abspath(in_path)
+
+
+def dir_not_empty(in_path: AnyStr) -> AnyStr:
+    abs_path = dir_exists(in_path)
+    contents = os.listdir(abs_path)
+    if not contents or len(contents) == 0:
+        raise argparse.ArgumentTypeError(f"Directory '{abs_path}' is empty")
+    return abs_path
+
+
+def parse_build_type(build_type: str) -> BuildType:
+    try:
+        return BuildType(build_type.strip())
+    except KeyError:
+        return BuildType.Unknown
 
 
 ################################################################################
 # | PACKAGING RELEASE |
 ################################################################################
 
-def package_release(args: argparse.Namespace):
-    file_dir = os.path.dirname(__file__)
-    zip_out_path = os.path.join(file_dir, "build", __zip_file__)
+VERSION: str
+BUILD_TYPE: BuildType
+OUTPUT_DIR: str
+MODS_DIR: str
+SDK_INSTALL_DIR: str
+ZIP_FILE_NAME: str
+ZIP_FILE_OUT: str
 
-    sdk_build_dir = args.sdk_build_dir
-    plugin_native_dll = args.plugins_native_dll
 
-    ZIP_PLUGINS_DIR = os.path.join("Binaries", "Plugins")
-    ZIP_MODS_DIR = os.path.join("Mods")
+def package_dir_into_zip(
+        dir_path: str,
+        zip_file: ZipFile,
+        base_path: str = "",
+) -> None:
+    """
+    Packages the provided directory into the provided zip file whilst mirroring the input
+    directory structure.
+    :param dir_path: Input directory
+    :param zip_file: Output zip file
+    :param base_path: Prefix path to prepend to output files
+    """
+    for root, _, files in os.walk(dir_path):
+        for file in files:
+            abs_path = path.join(root, file)
+            rel_path = path.relpath(abs_path, dir_path)
+            out_path = path.join(base_path, rel_path)
+            zip_file.write(abs_path, arcname=out_path)
 
-    with ZipFile(str(zip_out_path), "w", ZIP_DEFLATED, compresslevel=9) as zip_file:
 
-        # Copy src directory
-        for root, _, files in os.walk("./src"):
-            root_path_parts = os.path.normpath(root).split(os.sep)
-            if not args.include_hidden and any(p in root_path_parts for p in __exclude_paths__):
-                continue
-
-            for file in files:  # Installed into ./Mods/
-                abs_file_path = os.path.join(root, file)
-                src_rel_file_path = os.path.relpath(abs_file_path, file_dir)
-                mods_rel_path = os.path.join(ZIP_MODS_DIR, os.path.relpath(src_rel_file_path, "src"))
-                zip_file.write(abs_file_path, arcname=mods_rel_path)
-
-        # Copy SDK Build Files (Mirrors input directory); Installed into ./Binaries/Plugins
-        for root, _, files in os.walk(sdk_build_dir):
-            for file in files:
-                abs_file_path = os.path.join(root, file)
-                rel_file_path = os.path.relpath(str(abs_file_path), sdk_build_dir)
-                plugin_rel_path = os.path.join(ZIP_PLUGINS_DIR, str(rel_file_path))
-                zip_file.write(str(abs_file_path), arcname=plugin_rel_path)
-
-        # https://github.com/Ry0511/plugin_loader (apple1417's doesn't work here)
-        # Installed into ./Binaries/
-        native_out_path = os.path.join("Binaries", os.path.basename(plugin_native_dll))
-        zip_file.write(plugin_native_dll, arcname=native_out_path)
-
-        # Environment File (optional); Installed into ./Binaries/Plugins
-        ENV_FILE_NAME = "unrealsdk.env"
-        env_file = os.path.join(file_dir, ENV_FILE_NAME)
-        if os.path.isfile(env_file):
-            zip_file.write(env_file, arcname=os.path.join(ZIP_PLUGINS_DIR, ENV_FILE_NAME))
+def package_release():
+    with ZipFile(ZIP_FILE_OUT, "w", ZIP_DEFLATED, compresslevel=9) as zip_file:
+        package_dir_into_zip(MODS_DIR, zip_file, base_path="Mods")
+        package_dir_into_zip(SDK_INSTALL_DIR, zip_file)
 
 
 ################################################################################
@@ -83,53 +94,38 @@ def package_release(args: argparse.Namespace):
 ################################################################################
 
 if __name__ == "__main__":
-    import traceback
-    import tomllib
     from argparse import ArgumentParser
 
     parser = ArgumentParser(description="Prepares a .zip release")
-
-    # SDK Build Directory (best to use the install directory)
-    parser.add_argument(
-        "sdk_build_dir",
-        type=is_valid_dir,
-        help="pyunreal sdk build/install directory; "
-             "Installed in ./Binaries/Plugins"
-    )
-
-    # Usually just dsound.dll but others do exist
-    parser.add_argument(
-        "plugins_native_dll",
-        type=is_valid_file,
-        help="pyunreal sdk build/install directory; "
-             "Installed in ./Binaries"
-    )
-
-    # Skips over __exclude_paths__ when packaging ./src
-    parser.add_argument(
-        "--include-hidden",
-        help="Whether or not to include 'unrealsdk' "
-             "and 'pyunrealsdk' in the packaged mod "
-             "directory",
-        action="store_true",
-        default=False,
-    )
-    args: argparse.Namespace = parser.parse_args()
-
-    # Load toml info
-    with open(__file__.replace(".py", ".toml"), 'rb') as f:
-        data = tomllib.load(f)
-        __version__ = data["Project"]["version"]
-        __author__ = data["Project"]["author"]
-
-        if args.include_hidden:
-            __zip_file__ = f"PythonSDK-Dev-{__version__}.zip"
-        else:
-            __zip_file__ = f"PythonSDK-{__version__}.zip"
+    parser.add_argument("--version", required=True)
+    parser.add_argument("--sdk-install-dir", type=dir_not_empty, required=True)
+    parser.add_argument("--mods-dir", type=dir_not_empty, required=True)
+    parser.add_argument("--build-type", type=parse_build_type, required=True)
+    parser.add_argument("--output-dir", type=dir_exists, required=True)
 
     try:
-        package_release(args)
-    except Exception as err:
-        print("Error: ", err, "\n")
-        traceback.print_exc()
-        sys.exit(1)
+        args: argparse.Namespace = parser.parse_args()
+    except Exception as e:
+        print("ERROR PARSING ARGS: ", e)
+        exit(1)
+
+    VERSION = args.version
+    BUILD_TYPE = args.build_type
+    OUTPUT_DIR = args.output_dir
+    MODS_DIR = args.mods_dir
+    SDK_INSTALL_DIR = args.sdk_install_dir
+    ZIP_FILE_NAME = f"PythonSDK{BUILD_TYPE.file_prefix()}-{VERSION}.zip"
+    ZIP_FILE_OUT = path.join(OUTPUT_DIR, ZIP_FILE_NAME)
+
+    print(f"VERSION        : {VERSION}")
+    print(f"BUILD_TYPE     : {BUILD_TYPE}")
+    print(f"OUTPUT_DIR     : {OUTPUT_DIR}")
+    print(f"MODS_DIR       : {MODS_DIR}")
+    print(f"SDK_INSTALL_DIR: {SDK_INSTALL_DIR}")
+    print(f"ZIP_FILE_NAME  : {ZIP_FILE_NAME}")
+    print(f"ZIP_FILE_OUT   : {ZIP_FILE_OUT}")
+
+    print("\n[PACKAGING RELEASE]")
+    print(OUTPUT_DIR)
+    package_release()
+    print(ZIP_FILE_OUT)
