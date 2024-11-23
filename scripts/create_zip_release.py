4d4e5a6ccf41af0c626cf015faad3be5de532f18
Ryan Thomas Payne <50511769+Ry0511@users.noreply.github.com>
2024-11-23 17:04:35 +0000

    Added optional post build script to unpackage zips directly to the users Borderlands directory.

---
+++ scripts/create_zip_release.py
@@ -0,0 +1,141 @@
+import os
+import argparse
+from enum import Enum
+from os import path
+from typing import AnyStr
+from typing import overload
+from types import SimpleNamespace
+from zipfile import ZipFile, ZIP_DEFLATED, ZIP_STORED
+
+
+class BuildType(Enum):
+    Empty = ""
+    Debug = "Debug"
+    Release = "Release"
+    Unknown = "Custom"
+
+
+    def file_prefix(self):
+        if self is BuildType.Empty:
+            return ""
+        else:
+            return f"-{self.name}"
+
+
+################################################################################
+# | UTILITY |
+################################################################################
+
+
+def init_args_from_cmake() -> argparse.Namespace | None:
+    from argparse import ArgumentParser
+
+    def is_valid_file(in_path: AnyStr) -> AnyStr:
+        if not path.isfile(path.abspath(in_path)):
+            raise argparse.ArgumentTypeError(f"File '{in_path}' doesn't exist")
+        return path.abspath(in_path)
+
+
+    def dir_exists(in_path: AnyStr) -> AnyStr:
+        if not path.isdir(path.abspath(in_path)):
+            raise argparse.ArgumentTypeError(f"Directory '{in_path}' doesn't exist")
+        return path.abspath(in_path)
+
+
+    def dir_not_empty(in_path: AnyStr) -> AnyStr:
+        abs_path = dir_exists(in_path)
+        contents = os.listdir(abs_path)
+        if not contents or len(contents) == 0:
+            raise argparse.ArgumentTypeError(f"Directory '{abs_path}' is empty")
+        return abs_path
+
+
+    def parse_build_type(build_type: str) -> BuildType:
+        try:
+            return BuildType(build_type.strip())
+        except KeyError:
+            return BuildType.Unknown
+
+
+    parser = ArgumentParser(description="Prepares a .zip release")
+    parser.add_argument("--version", required=True)
+    parser.add_argument("--install-dir", type=dir_not_empty, required=True)
+    parser.add_argument("--build-type", type=parse_build_type, required=True)
+    parser.add_argument("--output-dir", type=dir_exists, required=True)
+
+    try:
+        args: argparse.Namespace = parser.parse_args()
+        return args
+    except Exception as e:
+        print("[ERROR] ~ ", e)
+        return None
+
+
+def parse_args_from_cmake(args: argparse.Namespace | None = None) -> SimpleNamespace:
+    if args is None:
+        args = init_args_from_cmake()
+        if args is None:
+            exit(1)
+
+    zip_file_name = f"PythonSDK{args.build_type.file_prefix()}-{args.version}.zip"
+
+    cfg = SimpleNamespace()
+    cfg.version = args.version
+    cfg.build_type = args.build_type
+    cfg.output_dir = args.output_dir
+    cfg.install_dir = args.install_dir
+    cfg.zip_file_name = zip_file_name
+    cfg.zip_file_out = path.join(args.output_dir, zip_file_name)
+    return cfg
+
+
+################################################################################
+# | PACKAGING RELEASE |
+################################################################################
+
+def package_dir_into_zip(
+        dir_path: str,
+        zip_file: ZipFile,
+        base_path: str = "",
+) -> None:
+    """
+    Packages the provided directory into the provided zip file whilst mirroring the input
+    directory structure.
+    :param dir_path: Input directory
+    :param zip_file: Output zip file
+    :param base_path: Prefix path to prepend to output files
+    """
+    for root, _, files in os.walk(dir_path):
+        for file in files:
+            abs_path = path.join(root, file)
+            rel_path = path.relpath(abs_path, dir_path)
+            out_path = path.join(base_path, rel_path)
+            zip_file.write(abs_path, arcname=out_path)
+
+
+def package_release(cfg: SimpleNamespace):
+    compression = ZIP_DEFLATED if cfg.build_type is not BuildType.Debug else ZIP_STORED
+    with ZipFile(cfg.zip_file_out, "w", compression, compresslevel=9) as zip_file:
+        package_dir_into_zip(cfg.install_dir, zip_file)
+
+
+################################################################################
+# | ENTRY |
+################################################################################
+
+if __name__ == "__main__":
+
+    cfg: SimpleNamespace = parse_args_from_cmake()
+
+    print("\n[CONFIGURATION]")
+    print(f"VERSION        : {cfg.version}")
+    print(f"BUILD_TYPE     : {cfg.build_type}")
+    print(f"OUTPUT_DIR     : {cfg.output_dir}")
+    print(f"SDK_INSTALL_DIR: {cfg.install_dir}")
+    print(f"ZIP_FILE_NAME  : {cfg.zip_file_name}")
+    print(f"ZIP_FILE_OUT   : {cfg.zip_file_out}")
+
+    package_release(cfg)
+    zip_stats = os.stat(cfg.zip_file_out)
+    zip_size = zip_stats.st_size / (1024 * 1024)
+    print(f"Packaged zip release ({zip_size:.2f}mb)")
---
+++ scripts/post_create_zip_release.py
@@ -0,0 +1,87 @@
+import os
+import shutil
+import sys
+import tomllib
+from os import path
+from types import SimpleNamespace
+import zipfile
+
+from create_zip_release import parse_args_from_cmake
+
+
+def validate_files(cfg: SimpleNamespace) -> bool:
+    valid_files = True
+
+
+    def is_valid_dir(p, msg="Directory doesn't exist") -> bool:
+        if not path.isdir(p):
+            print(msg, f"; '{p}'")
+            return False
+        return True
+
+
+    if (  # Just ensure we have the right borderlands directory
+            not is_valid_dir(cfg.bl_root_dir, "Borderlands directory doesn't exist;")
+            or not is_valid_dir(path.join(cfg.bl_root_dir, "Binaries"))
+            or not is_valid_dir(path.join(cfg.bl_root_dir, "WillowGame"))
+    ):
+        valid_files = False
+
+    if not path.isfile(cfg.zip_file_out):
+        print(f"Zip file doesn't exist; '{cfg.zip_file_out}'")
+
+    # These don't need to exist
+    for p in cfg.bl_delete_list:
+        print(f"[WARNING] Path is on delete list; '{p}'")
+
+    return valid_files
+
+
+def parse_toml(cfg: SimpleNamespace, toml_file) -> None:
+    with open(toml_file, 'rb') as f:
+        toml = tomllib.load(f)
+        cfg.bl_root_dir = path.abspath(toml['borderlands']['root_dir'])
+        cfg.bl_delete_list = list(map(lambda p: path.join(cfg.bl_root_dir, path.normpath(p)),
+                                      toml['borderlands']['delete_list']))
+        cfg.bl_dry_run = bool(toml['borderlands']['dry_run'])
+
+
+if __name__ == "__main__":
+
+    cur_dir = path.dirname(__file__)
+    toml_file = path.join(cur_dir, str(path.basename(__file__)).replace('.py', '.toml'))
+
+    if not path.isfile(toml_file):
+        print(f"Toml file: '{toml_file}' doesn't exist skipping post package.")
+        exit(0)
+
+    cfg: SimpleNamespace = parse_args_from_cmake()
+    parse_toml(cfg, toml_file)
+    print(cfg)
+
+    if not validate_files(cfg) or bool(cfg.bl_dry_run):
+        print("[DRY_RUN] Exiting...")
+        sys.exit(0)
+
+    # Delete files and or directories
+    print("\nDeleting files and directories in delete list...")
+    for p in cfg.bl_delete_list:
+        try:
+            if path.isdir(p):
+                shutil.rmtree(p)
+                print(f"Deleted directory; '{p}'")
+
+            elif path.isfile(p):
+                os.remove(p)
+                print(f"Deleted file; '{p}'")
+
+        except Exception as err:
+            print(f"Error deleting path; '{p}'")
+            print(err)
+
+    # Will replace existing.
+    print("\nExtracting zip file...")
+    print(cfg.zip_file_out)
+    print(cfg.bl_root_dir)
+    with zipfile.ZipFile(cfg.zip_file_out) as zip_file:
+        zip_file.extractall(cfg.bl_root_dir)
