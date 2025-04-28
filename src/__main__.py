f3119055e04944843369f377ff3982657f0a39b9
Ryan Thomas Payne <50511769+Ry0511@users.noreply.github.com>
2025-04-28 20:10:26 +0100

    Add editor detection by probing system args

--- src/__main__.py
+++ src/__main__.py
@@ -92,7 +92,7 @@ def is_valid_mod_path(p: Path) -> bool:
         return False
 
     content: list[Path] = list(map(lambda x: x.name, p.iterdir()))
-    if ".pysdk_exclude" in content or "__init__.py" not in content:
+    if ".sdk_skip" in content or "__init__.py" not in content:
         return False
 
     return True
@@ -130,24 +130,75 @@ def load_mods_from_dir(mod_dir: Path) -> (int, int):
     return mods_loaded, mods_failed
 
 
+def get_launch_args() -> list[str]:
+    # Bit more complex than it needs to be tbh
+
+    try:
+        import ctypes
+        from ctypes import wintypes
+
+        # https://learn.microsoft.com/en-us/windows/win32/api/processenv/nf-processenv-getcommandlinew
+        k32 = ctypes.windll.kernel32
+        get_cmd_line_w = k32.GetCommandLineW
+        get_cmd_line_w.restype = wintypes.LPWSTR
+
+        # https://learn.microsoft.com/en-us/windows/win32/api/shellapi/nf-shellapi-commandlinetoargvw
+        shell32 = ctypes.windll.shell32
+        command_line_to_argv_w = shell32.CommandLineToArgvW
+        command_line_to_argv_w.argtypes = [wintypes.LPCWSTR, ctypes.POINTER(ctypes.c_int)]
+        command_line_to_argv_w.restype = ctypes.POINTER(wintypes.LPWSTR)
+
+        # Parse args
+        cmd_line = get_cmd_line_w()
+        argc = ctypes.c_int(0)
+        argv = command_line_to_argv_w(cmd_line, argc)
+        args: list[str] = [argv[i] for i in range(argc.value)]
+
+        # https://learn.microsoft.com/en-us/windows/win32/api/shellapi/nf-shellapi-commandlinetoargvw
+        local_free = k32.LocalFree
+        local_free.argtypes = [wintypes.HLOCAL]
+        local_free.restype = wintypes.HLOCAL
+        local_free(argv)
+
+        return args
+
+    except ImportError as ex:
+        logging.error("Failed to import 'ctypes' module; Skipping checking args...")
+        logging.error(ex)
+        return []
+
+
 ################################################################################
 # | MAIN CODE BLOCK |
 ################################################################################
 
-# Might feel odd to iterate over this twice but importing a mod from one directory that relies on
-# another will cause issues. So before we do any importing we ensure all possible imports can
-# resolve.
-all_mod_directories = get_mod_directories()
-for p in all_mod_directories:
-    if p not in sys.path:
-        logging.info(f"Adding directory to search path: '{p}'")
-        sys.path.append(str(p))
+args = list(map(lambda arg: arg.lower(), get_launch_args()[1:]))
 
-import keybinds  # noqa
-from mods_base import mod_list
+if "-editor" not in args:
+    logging.info("Loading mods normally...")
 
-# Load all user mods
-for p in all_mod_directories:
-    load_mods_from_dir(p)
+    # Might feel odd to iterate over this twice but importing a mod from one directory that relies on
+    # another will cause issues. So before we do any importing we ensure all possible imports can
+    # resolve.
+    all_mod_directories = get_mod_directories()
+    for p in all_mod_directories:
+        if p not in sys.path:
+            logging.info(f"Adding directory to search path: '{p}'")
+            sys.path.append(str(p))
 
-mod_list.register_base_mod()
+    import keybinds  # noqa
+    from mods_base.mod_list import register_base_mod
+
+    failed: int = 0
+    succeeded: int = 0
+
+    # Load all user mods
+    for p in all_mod_directories:
+        good, bad = load_mods_from_dir(p)
+        succeeded += good
+        failed += bad
+
+    register_base_mod()
+    logging.info(f"Loaded {succeeded} mods successfully, {failed} failed.")
+else:
+    logging.info("Editor launch argument provided; Not loading mods.")
